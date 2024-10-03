c00d99d4dc135e6e51c499385c76e083bee94490
clarasia <112596540+ReinhardBatubara@users.noreply.github.com>
2024-10-03 14:25:51 +0700

    button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,10 +22,7 @@
                     <img src="Tuperware.jpeg" alt="Tumbler Tupperware">
                     <h2>Tumbler Tupperware</h2>
                     <button class="add-to-cart">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
-                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-                            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
-                        </svg>
+                    @include('layout.button_add')
                     </button>
                           <p>Rp 100.000</p>
                 </div>
@@ -34,50 +31,40 @@
                     <h2>FlashDisk</h2>
                     <p>Rp 50.000</p>
                     <button class="add-to-cart">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
-                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-                            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
-                          </svg></button>
+                    @include('layout.button_add')
+                    </button>
                 </div>
                 <div class="product">
                     <img src="image/laptop.jpg" alt="laptop">
                     <h2>laptop</h2>
                     <p>Rp 20.000.000</p>
                     <button class="add-to-cart">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
-                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-                            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
-                          </svg></button>
+                    @include('layout.button_add')
+                    </button>
                 </div>
                 <div class="product">
                     <img src="image/kue.jpg" alt="kue">
                     <h2>kue</h2>
                     <p>Rp 50.000</p>
                     <button class="add-to-cart">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
-                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-                            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
-                          </svg></button>
+                    @include('layout.button_add')
+                    </button>
                 </div>
                 <div class="product">
                     <img src="image/mouse.jpg" alt="mouse">
                     <h2>mouse</h2>
                     <p>Rp 250.000</p>
                     <button class="add-to-cart">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
-                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-                            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
-                          </svg></button>
+                    @include('layout.button_add')
+                    </button>
                 </div>
                 <div class="product">
                     <img src="image/flashdisk.jpg" alt="flashdisk">
                     <h2>flashdisk</h2>
                     <p>Rp 30.000</p>
                     <button class="add-to-cart">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
-                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-                            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
-                          </svg></button>
+                    @include('layout.button_add')
+                    </button>
                 </div>
             </div>
         </main>
