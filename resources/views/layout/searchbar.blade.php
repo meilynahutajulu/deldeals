a915ceebb6c13bb393263c704774167b0e151cb9
clarasia <112596540+ReinhardBatubara@users.noreply.github.com>
2024-10-03 14:09:48 +0700

    new

---
+++ resources/views/layout/searchbar.blade.php
@@ -0,0 +1,9 @@
+<div class="search-bar">
+                <input type="text" placeholder="Cari...">
+                <div class="icons">
+                    <i class="fas fa-bell"></i>
+                    <i class="fas fa-user"></i>
+                    <i class="fas fa-shopping-cart"></i>
+                    <i class="fas fa-info-circle"></i>
+                </div>
+            </div>
