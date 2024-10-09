eb11f95ee5e8bcd324e05722da90e572c8cb6e6c
clarasia <112596540+ReinhardBatubara@users.noreply.github.com>
2024-10-09 20:31:05 +0700

    Update

--- resources/views/tokosaya.blade.php
+++ resources/views/tokosaya.blade.php
@@ -20,6 +20,29 @@
         <main class="content">
             <!-- Search bar -->
         @include('layout.searchbar')
+        
+        <!-- Product grid -->
+        <div class="product-grid">
+                <div class="product">
+                    <img src="Tuperware.jpeg" alt="Tumbler Tupperware">
+                    <h2>Tumbler Tupperware</h2>
+                    <p>Rp 100.000</p>
+                    <button class="add-to-cart">@include('layout.button_add')</button>
+                          
+                </div>
+                <div class="product">
+                    <img src="FlashDisk.jpeg" alt="FlashDisk">
+                    <h2>FlashDisk</h2>
+                    <p>Rp 50.000</p>
+                    <button class="add-to-cart">@include('layout.button_add')</button>
+                </div>
+                <div class="product">
+                    <img src="image/laptop.jpg" alt="laptop">
+                    <h2>laptop</h2>
+                    <p>Rp 20.000.000</p>
+                    <button class="add-to-cart">@include('layout.button_add')</button>
+                </div>
+            </div>
 
         </main>
     </div>
