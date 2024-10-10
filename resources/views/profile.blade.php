e57d7b31ba4c63fc8ec225e68fd0754799a1784f
clarasia <112596540+ReinhardBatubara@users.noreply.github.com>
2024-10-10 08:06:48 +0700

    Update

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -60,20 +60,23 @@
     </div>
 </div>
             <h1>Profil Pengguna</h1>
-            <div class="profile-card">
-                <div class="profile-picture">
-                    <img src="User profile.png" alt="Profile Picture">
-                </div>
-                <div class="profile-details">
-                    <p><strong>Username:</strong> @bernadyaaa_1bulan</p>
-                    <p><strong>Nama:</strong> Bernadya Simangunsong</p>
-                    <p><strong>NIM:</strong> 12S22000</p>
-                    <p><strong>Email:</strong> bernadyassssssss123x@gmail.com</p>
-                    <p><strong>No. Handphone:</strong> 08131234567</p>
-                    <button class="edit-btn">Edit</button>
-                </div>
+        <div class="profile-card">
+            <div class="profile-picture">
+                <img src="User profile.png" alt="Profile Picture">
             </div>
-            
+            <div class="profile-details">
+                <p><strong>Username:</strong> @bernadyaaa_1bulan</p>
+                <p><strong>Nama:</strong> Bernadya Simangunsong</p>
+                <p><strong>NIM:</strong> 12S22000</p>
+                <p><strong>Email:</strong> bernadyassssssss123x@gmail.com</p>
+                <p><strong>No. Handphone:</strong> 08131234567</p>
+            </div>
+        </div>
+        <button class="edit-btn">Edit
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+                <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708z"/>
+            </svg>
+        </button>
         </main>
     </div>
 
