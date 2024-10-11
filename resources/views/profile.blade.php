58079b43794d9a256d0c44dfd093b1cfecd2487f
clarasia <112596540+ReinhardBatubara@users.noreply.github.com>
2024-10-11 15:55:40 +0700

    Update Fitur profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile Page</title>
-    <link rel="stylesheet" href="css/profile.css">  
+    <link rel="stylesheet" href="css/profile.css"> 
 </head>
 <body>
     <!-- wallpaper -->
@@ -26,15 +26,17 @@
             </div>
                 <!-- Profile Icon with Dropdown -->
         <!-- <div class="profile-dropdown" style="position: relative;"> -->
+        <a href="{{ url('/profile') }}">
             <div class="profile-icon" onclick="toggleDropdown()" style="cursor: pointer;">
                 <!-- Profile Icon (SVG) -->
-                <a href="{{ url('/profile') }}">
+                
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                     <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
                     <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
                 </svg>
-                </a>
-            </div>    
+                
+            </div>
+            </a> 
             <!-- Dropdown Menu -->
             <!-- <div id="dropdown-box" class="dropdown-content" style="display: none; position: absolute; top: 40px; right: 0; background-color: white; box-shadow: 0px 8px 16px rgba(0,0,0,0.1); padding: 10px; border-radius: 5px; z-index: 100;">
                 <a href="#">Profil</a>
@@ -70,11 +72,15 @@
                 <p><strong>No. Handphone:</strong> 08131234567</p>
             </div>
         </div>
-        <button class="edit-btn">Edit
+        <a href="{{ url('/editprofile') }}">
+        <button class="edit-btn">
+        Edit
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
                 <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708z"/>
             </svg>
+            
         </button>
+        </a>
         </main>
     </div>
 
