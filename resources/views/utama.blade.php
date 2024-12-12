6300317bd8ad5404926bf69c929ecd892cfb875c
septianasimanullang11 <septianasimanullang@gmail.com>
2024-12-12 14:09:04 +0700

    updatee

--- resources/views/utama.blade.php
+++ resources/views/utama.blade.php
@@ -15,28 +15,23 @@
         <!-- Main Content -->
         <main class="content">
             
-                <div class="product-grid">
-                    @if($item->isEmpty())
-                        @if(request('search'))
-                            <p>Produk dengan nama "{{ request('search') }}" tidak ditemukan.</p>
-                        @else
-                            <p>Tidak ada produk yang tersedia.</p>
-                        @endif
-                    @else
-                        @foreach ($item as $items)
-                            <div class="image-container">
-                                <img src="{{ asset('storage/' . $items->image) }}" alt="{{ $items->name }}">
-                            </div>
-                            <h3>{{ $items->name }}</h3>
-                            <p>Rp {{ number_format($items->price, 2, ',', '.') }}</p>
-                            <button class="add-to-cart">+</button>
-                        @endforeach
-                    @endif
-                </div>
-           
-        </main>
-    </div>
-
-    <script src="{{ asset('js/app.js') }}"></script>
-</body>
-</html>
+        <div class="product-grid">
+    @if($item->isEmpty())
+        @if(request('search'))
+            <p>Produk dengan nama "{{ request('search') }}" tidak ditemukan.</p>
+        @else
+            <p>Tidak ada produk yang tersedia.</p>
+        @endif
+    @else
+        @foreach ($item as $items)
+        <div class="product-card">
+            <div class="image-container">
+                <img src="{{ asset('storage/' . $items->image) }}" alt="{{ $items->name }}">
+            </div>
+            <h3>{{ $items->name }}</h3>
+            <p>Rp {{ number_format($items->price, 2, ',', '.') }}</p>
+            <button class="add-to-cart">+</button>
+        </div>
+        @endforeach
+    @endif
+</div>
