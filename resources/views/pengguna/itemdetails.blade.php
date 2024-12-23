b2630f9250c71f73ec68aaaee6bad32d101ee003
septianasimanullang11 <septianasimanullang@gmail.com>
2024-12-23 10:45:25 +0700

    item details

--- resources/views/pengguna/itemdetails.blade.php
+++ resources/views/pengguna/itemdetails.blade.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Detail Barang</title>
     <!-- Import Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Import Custom CSS -->
-    <link rel="stylesheet" href="{{ asset('css/layou/itemdetails.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/layout/itemdetails.css') }}">
 </head>
 <body>
     <!-- Wallpaper Background -->
@@ -41,7 +41,7 @@
                                     <div class="col-md-8">
                                         <p><strong>Nama Barang:</strong> {{ $selectedItem['name'] }}</p>
                                         <p><strong>Nama Penjual:</strong> {{ $selectedItem['seller'] }}</p>
-                                        <p class="fw-bold text-danger"><strong>Harga:</strong> Rp {{ number_format($selectedItem['price'], 0, ',', '.') }}</p>
+                                        <p class="text-dark"><strong>Harga:</strong> Rp {{ number_format($selectedItem['price'], 0, ',', '.') }}</p>
                                         <p><strong>Deskripsi:</strong> {{ $selectedItem['description'] }}</p>
                                         <p><strong>Nomor Telepon: </strong> {{$selectedItem['contact']}} </p>
                                     </div>
